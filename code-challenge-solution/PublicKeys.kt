4040e2403e6bf6267eac5d6b46418d14f1ffdb61
Hardik Trivedi <hardik.trivedi_11@yahoo.com>
2023-08-30 18:37:20 +0100

    Replaced helper files and added coding challenge file

--- code-challenge-solution/PublicKeys.kt
+++ code-challenge-solution/PublicKeys.kt
@@ -4,7 +4,8 @@ internal const val PINNED_HOST_NAME = "publicobject.com"
 
 internal val correctPins = arrayOf(
     "sha256/tK20XqQX9/O1Pye/fEIvMnLoaXxqMt+BAA1wHm7419A=",
-    "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0="
+    "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=",
+    "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M="
 )
 
 internal val incorrectPins = arrayOf(
